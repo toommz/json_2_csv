5157d9c8874ed69b014831ee7f01490fddaccbc5
Thomas Battiston <thomas.battiston@gmail.com>
2021-06-12 17:36:20 +0200

    chore: init repo

---
+++ lib/json_2_csv.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "json_2_csv/version"
+
+module Json2Csv
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/json_2_csv_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Json2Csv do
+  it "has a version number" do
+    expect(Json2Csv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
